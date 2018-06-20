8f95b1ac378db18cf12cb9141eea10cc10ec913e
Prashant Shishodia <33901783+pshishod2645@users.noreply.github.com>
2018-06-20 18:27:15 +0530

    Update Floyd-Warshall.cpp

--- Floyd-Warshall.cpp
+++ Floyd-Warshall.cpp
@@ -13,9 +13,9 @@ int main()
 
 	for(int i=1;i<=n;++i)
 		for(int j=1;j<=n;++j)
-			dis[i][j] = INF;          // setting the distance as infinity 
-  for(int i=1;i<=n;++i)
-    dis[i][i] = 0;              // graph is 1-indexed
+			dis[i][j] = INF;         // setting the distance as infinity 
+  	for(int i=1;i<=n;++i)
+  	  dis[i][i] = 0;              		// graph is 1-indexed
 
 	for(int i=0;i<m;++i)
 	{
@@ -24,7 +24,7 @@ int main()
 		dis[x][y] = dis[y][x] = d;
 	}
 
-	for(int k=1;k<=n;++k)               // using intermediate verices only from the set {1, 2, 3, .. k}
+	for(int k=1;k<=n;++k)              	 // using intermediate verices only from the set {1, 2, 3, .. k}
 		for(int i=1;i<=n;++i)
 			for(int j=1;j<=n;++j)
 				dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
