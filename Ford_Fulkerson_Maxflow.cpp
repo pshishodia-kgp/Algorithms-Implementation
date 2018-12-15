b57dc47339c033c260184fdc797cf181b308b84a
Prashant Shishodia <33901783+pshishod2645@users.noreply.github.com>
2018-12-15 23:04:51 +0530

    Update Ford_Fulkerson_Maxflow.cpp

--- Ford_Fulkerson_Maxflow.cpp
+++ Ford_Fulkerson_Maxflow.cpp
@@ -7,51 +7,67 @@ using namespace std;
 const int N = 2010;
 const int inf = 1e9;
 
-vector<int> g[N];
-vector<pair<int, int> > edge[N];
-int n, m, cap[N][N], a[N];
+int n, m, a[N];
 long long ans;
 
-int bfs(int s, int t, vector<int> &parent){
-	fill(parent.begin(), parent.end(), -1);
-	parent[s] = -2;
+struct fordFulkerson{
+	int n, source, sink;
+	vector<int> parent;
+	vector<vector<int> > cap, g;
 
-	queue<pair<int, int> > q;
-	q.push({s, inf});
-
-	while(!q.empty()){
-		int v = q.front().first;
-		int flow = q.front().second; 
-		q.pop();
-
-		for(int u : g[v])if(parent[u] == -1 && cap[v][u] > 0){
-			int new_flow = min(flow, cap[v][u]);
-			parent[u] = v;
-
-			if(u == t)return new_flow;
-			q.push({u, new_flow});
-		}
+	fordFulkerson(int size){
+		n = size;
+		parent = vector<int> (n + 1);
+		cap = vector<vector<int > > (n + 1, vector<int>(n + 1));
+		g = vector<vector<int> > (n + 1);
 	}
-	return 0;
-}
 
-long long maxflow(int s, int t){
-	long long flow = 0;
-	vector<int> parent(n + m + 2);
-	int new_flow = 0;
-
-	while(new_flow = bfs(s, t, parent)){
-		flow += new_flow;
-		int curr = t;
-		while(curr != s){
-			int prev = parent[curr];
-			cap[prev][curr] -= new_flow;
-			cap[curr][prev] += new_flow;
-			curr = prev;
-		}
+	void addEdge(int u, int v, int w){
+		g[u].push_back(v);
+		g[v].push_back(u);
+		cap[u][v] = w;
 	}
-	return flow;
-}
+
+	long long maxflow(){
+		long long flow = 0, new_flow = 0;
+
+		while(new_flow = bfs()){
+			flow += new_flow;
+			
+			int curr = sink;
+			while(curr != source){
+				int prev = parent[curr];
+				cap[prev][curr] -= new_flow;
+				cap[curr][prev] += new_flow;
+				curr = prev;
+			}
+		}
+		return flow;
+	}
+
+	int bfs(){
+		fill(parent.begin(), parent.end(), -1);
+		parent[source] = -2;
+
+		queue<pair<int, int> > q;
+
+		q.push({source, inf});
+
+		while(!q.empty()){
+			int v = q.front().first;
+			int flow = q.front().second; q.pop();
+
+			for(int u : g[v])if(parent[u] == -1 && cap[v][u] > 0){
+				int new_flow = min(flow, cap[v][u]);
+				parent[u] = v;
+
+				if(u == sink)return new_flow;
+				q.push({u, new_flow});
+			}
+		}
+		return 0;
+	}
+};
 
 int main(){
 	ios_base::sync_with_stdio(0);
@@ -59,28 +75,24 @@ int main(){
 
 	cin>>n>>m;
 
+	fordFulkerson flow(n + m + 2);
+
 	for(int i=1;i<=n;++i){
 		cin>>a[i];
-		g[0].push_back(i);
-		g[i].push_back(0);
-		cap[0][i] = a[i];
+		flow.addEdge(0, i, a[i]);
 	}
 
 	for(int i=1;i<=m;++i){
 		int u, v, w;
 		cin>>u>>v>>w;
 
-		cap[u][n + i] = cap[v][n + i] = inf;
-		g[u].push_back(n + i); g[v].push_back(n + i);
-		g[n + i].push_back(u); g[n + i].push_back(v);
-
-		cap[n + i][n + m + 1] = w;
-		g[n + i].push_back(n + m + 1);
-		g[n + m + 1].push_back(n + i);
+		flow.addEdge(u, n + i, inf); flow.addEdge(v, n + i, inf);
+		flow.addEdge(n + i, n + m + 1, w);
 
 		ans += w;
 	}
 
-	cout<<ans - maxflow(0, n + m + 1)<<"\n";
+	flow.source = 0; flow.sink = n + m + 1;
+	cout<<ans - flow.maxflow()<<"\n";
 	return 0;
 }
