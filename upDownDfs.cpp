a97e20b63021454cc65cbd38481d64bebdd81cde
Prashant Shishodia <33901783+pshishod2645@users.noreply.github.com>
2019-05-02 15:09:58 +0530

    Create upDownDfs.cpp

---
+++ upDownDfs.cpp
@@ -0,0 +1,60 @@
+/* 
+Problem Link : https://codeforces.com/contest/1156/problem/D
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2e5 + 5;
+int n, dp[N][2]; 
+
+vector<pair<int, int> > g[N]; 
+
+void basic_dfs(int v, int prnt){
+    for(auto p : g[v]){
+        int u = p.first, c = p.second; 
+        if(u == prnt)continue;
+
+        basic_dfs(u, v); 
+        if(c == 0) dp[v][0] += 1 + dp[u][0] + dp[u][1];
+        else dp[v][1] += 1 + dp[u][1]; 
+    }
+}
+
+void parental_dfs(int v, int prnt, int pcolor = -1){
+    if(v != 1){
+        if(pcolor == 0)dp[v][0] += 1 + dp[prnt][0] + dp[prnt][1] - (1 + dp[v][0] + dp[v][1]);
+        else dp[v][1] += 1 + dp[prnt][1] - (1 + dp[v][1]); 
+    }
+
+    for(auto p : g[v]){
+        int u = p.first, c = p.second; 
+        if(u == prnt)continue;
+
+        parental_dfs(u, v, c);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin>>n;
+    for(int i=0;i<n-1;++i){
+        int u, v, c; cin>>u>>v>>c; 
+        g[u].push_back({v, c}); 
+        g[v].push_back({u, c}); 
+    }
+    basic_dfs(1, 0);
+    parental_dfs(1, 0);
+
+    ll ans = 0;
+    for(int i=1;i<=n;++i){
+        ans += dp[i][0] + dp[i][1];
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
