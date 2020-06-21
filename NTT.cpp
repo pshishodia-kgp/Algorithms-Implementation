623934645e43d54ea2950750be97cc6b4e71d093
Prashant Shishodia <33901783+pshishod2645@users.noreply.github.com>
2020-06-21 22:00:56 +0530

    Update NTT.cpp

--- NTT.cpp
+++ NTT.cpp
@@ -1,50 +1,44 @@
-namespace FFT {
-//  `替代方案: $23068673( = 11 * 2 ^ {21} + 1)$, 原根为 $3$`
-const int  MOD = 998244353, PRIMITIVE_ROOT = 3; // `$3 * 2 ^ {18} + 1$`
-const int MAXB = 1 << 22;
-// int getMod(int downLimit) { // `或者现场自己找一个MOD`
-//   for (int c = 3; ; ++c) { int t = (c << 21) | 1;
-//     if (t >= downLimit && isPrime(t)) return t;
-// }}
-ll pow1(ll a, ll b)
-{
-  if (b == 0)  return 1;
-  ll ret = pow1(a, b / 2);
-  ret = (ret * ret) % MOD;
-  if (b & 1) ret = (a * ret) % MOD;
-  return ret;
-}
+namespace NTT{
+  // const int MOD = 998244353; // = 7 * 17 * 2^23 + 1, 2^22 = 4e6
+  const int PRIMITIVE_ROOT = 3, PRIMITIVE_ROOT_1 = inv(PRIMITIVE_ROOT); 
 
-int modInv(int a) { return a <= 1 ? a : (long long) (MOD - MOD / a) * modInv(MOD % a) % MOD; }
-void NTT(int P[], int n, int oper) {
-  for (int i = 1, j = 0; i < n - 1; i++) {
-    for (int s = n; j ^= s >>= 1, ~j & s;);
-    if (i < j) swap(P[i], P[j]);
-  }
-  for (int d = 0; (1 << d) < n; d++) {
-    int m = 1 << d, m2 = m * 2;
-    long long unit_p0 = pow1(PRIMITIVE_ROOT, (MOD - 1) / m2);
-    if (oper < 0) unit_p0 = modInv(unit_p0);
-    for (int i = 0; i < n; i += m2) {
-      long long unit = 1;
-      for (int j = 0; j < m; j++) {
-        int &P1 = P[i + j + m], &P2 = P[i + j];
-        int t = unit * P1 % MOD;
-        P1 = (P2 - t + MOD) % MOD; P2 = (P2 + t) % MOD;
-        unit = unit * unit_p0 % MOD;
+  void NTT(vector<int> &a, bool invert){
+    int n = a.size();
+    for(int i = 1, j = 0; i < n; ++i){            // precalculate reverse permutation
+      int pw = n>>1; 
+      for(; j & pw; pw >>= 1)j^= pw;
+      j^= pw; 
+      if(i < j)swap(a[i], a[j]); 
+    }
+
+    for(int len = 2; len <= n; len <<= 1){
+      int wn = binpow(invert? PRIMITIVE_ROOT_1 : PRIMITIVE_ROOT, (MOD - 1)/len), len2 = len/2; // precalculate powers
+
+      for(int i = 0;i < n; i += len){
+        int w = 1;                      // use precalculated powers
+        for(int j = 0; j < len2; ++j){
+          int u = a[i + j], v = 1ll * a[i + j + len2] * w % MOD; 
+          a[i + j] = (u + v >= MOD)? u + v - MOD : u + v; 
+          a[i + j + len2] = (u - v < 0)? u - v + MOD : u - v;  
+          w = 1ll * w * wn % MOD; 
+        }
       }
     }
+    if(invert){
+      int inv_n = inv(n); 
+      for(int &x : a)x = 1ll * x * inv_n % MOD; 
+    }
   }
-}
-vector<ll> mul(const vector<ll> &a, const vector<ll> &b) {
-  vector<ll> ret(max(0ll, (int) a.size() + (int) b.size() - 1), 0);
-  static int A[MAXB], B[MAXB], C[MAXB];
-  int len = 1; while (len < (int)ret.size()) len <<= 1;
-  for (int i = 0; i < len; i++) A[i] = i < (int)a.size() ? a[i] : 0;
-  for (int i = 0; i < len; i++) B[i] = i < (int)b.size() ? b[i] : 0;
-  NTT(A, len, 1); NTT(B, len, 1);
-  for (int i = 0; i < len; i++) C[i] = (long long) A[i] * B[i] % MOD;
-  NTT(C, len, -1); for (int i = 0, inv = modInv(len); i < (int)ret.size(); i++) ret[i] = (long long) C[i] * inv % MOD;
-  return ret;
-}
+
+  vector<int> mul(vector<int> a, vector<int> b){
+    int n = 1; 
+    while(n < a.size() + b.size())n <<= 1; 
+    a.resize(n); b.resize(n); 
+
+    NTT(a, false); NTT(b, false); 
+    for(int i = 0;i < n; ++i)a[i] = 1ll * a[i] * b[i] % MOD; 
+
+    NTT(a, true); 
+    return a; 
+  }
 }
