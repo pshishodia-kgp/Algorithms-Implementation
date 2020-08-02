a1cc99c4df3b3e1537e0fdc4cd2b3a0cc646a4f0
Prashant Shishodia <33901783+pshishod2645@users.noreply.github.com>
2020-08-02 22:06:09 +0530

    Update DynamicDSU.cpp

--- DynamicDSU.cpp
+++ DynamicDSU.cpp
@@ -1,27 +1,29 @@
+int cmp; 
+
 struct DynamicDSU{
     vector<int> p, sz; 
-    stack<int> st; 
+    stack<int> st;  
 
     DynamicDSU(int n){
-        p = sz = vector<int> (n); 
+        p = sz = vector<int> (n);
         for(int i = 0;i < n; ++i)p[i] = i, sz[i] = 1; 
     }
     inline int root(int x){
-        if(p[x] != x)return root(p[x]); 
+        while(x != p[x])x = p[x];      // no path compression
+        return p[x]; 
     }
     inline void merge(int x, int y){
-        x = root(x), y = root(y); 
+        x = root(x), y = root(y);  
         if(x == y)return ; 
         if(sz[x] < sz[y])swap(x, y); 
         p[y] = x; sz[x] += sz[y]; 
-        st.push(y); // y is connected to p[y]
+        cmp--; st.push(y); // y is connected to p[y]
     }
     inline void rollback(){
         int y = st.top(), x = p[y]; 
         sz[x] -= sz[y]; p[y] = y; 
-        st.pop(); 
+        st.pop(); cmp++; 
     }
     void rollback(int upto){
         while(st.size() > upto)rollback(); 
     }
-};
