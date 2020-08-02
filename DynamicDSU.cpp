5e6aac491ddecb0fd256403511a8af2dc60976b1
Prashant Shishodia <33901783+pshishod2645@users.noreply.github.com>
2020-08-02 23:27:07 +0530

    Update DynamicDSU.cpp

--- DynamicDSU.cpp
+++ DynamicDSU.cpp
@@ -28,3 +28,39 @@ struct DynamicDSU{
         while(st.size() > upto)rollback(); 
     }
 }dsu(N);
+
+struct DynamicDSUBipartite{
+    vector<int> p, sz, parity;  
+    stack<int> st, notBP;  
+
+    DynamicDSU(int n){
+        parity = p = sz = vector<int> (n); st.push(-1); notBP.push(0); 
+        for(int i = 0;i < n; ++i)p[i] = i, sz[i] = 1; 
+    }
+    inline pair<int, int> root(int x){
+        int d = 0; 
+        while(x != p[x])d^= parity[x], x = p[x];      // no path compression
+        return {p[x], d}; 
+    }
+    inline void merge(int x, int y){
+        auto px = root(x), py = root(y);  
+        x = px.first, y = py.first; 
+        if(x == y){
+            if(px.second == py.second){
+                st.push(x); notBP.push(1); 
+            }
+            return ;
+        } 
+        if(sz[x] < sz[y])swap(x, y); 
+        p[y] = x; sz[x] += sz[y]; parity[y] = px.second ^ py.second ^ 1; 
+        st.push(y); notBP.push(notBP.top()); // y is connected to p[y]
+    }
+    inline void rollback(){
+        int y = st.top(), x = p[y]; st.pop(); notBP.pop();
+        if(y == x)return ; 
+        sz[x] -= sz[y]; p[y] = y; parity[y] = 1; 
+    }
+    void rollback(int upto){
+        while(st.size() > upto)rollback(); 
+    }
+}dsu(N);
